50502c0d13c4f8d99fa51ea93aa67b0f1c0ecdea
Joshua Myatt <joshuamyatt@sky.com>
2022-03-20 23:11:19 +0000

    StickMan sheet working

--- game.py
+++ game.py
@@ -14,36 +14,72 @@ HEIGHT = 400
 STEP = 0.5
 GRAVITY = 0.7
 GROUND_Y = HEIGHT + 20
+SHEET_WIDTH = 1440
+SHEET_HEIGHT = 1480
+COLUMNS = 6
+ROWS = 5
+SHEET_URL = "https://i.imgur.com/GQy2YrF.png"
+time = 1
 
 class Wheel:
-    def __init__(self, pos, radius=10):
-        self.pos = pos
-        self.vel = Vector()
+    def __init__(self, SHEET_URL, WIDTH, HEIGHT, COLUMNS, ROWS, radius=50):
+        
+        self.SHEET_URL = simplegui.load_image(SHEET_URL)
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.COLUMNS = COLUMNS
+        self.ROWS = ROWS
+
+        self.frame_width = self.WIDTH/COLUMNS
+        self.frame_height = self.HEIGHT/ROWS
+        self.frame_centre_x = self.frame_width/2
+        self.frame_centre_y = self.frame_height/2
+        
+        self.frame_index = [4, 1]
+        
         self.radius = max(radius, 10)
-        self.colour = 'White'
-        self.img = simplegui.load_image("https://freesvg.org/img/8-Bit-sprite.png-1.png")
-        self.img_centre = (256, 256)
-        self.img_dims = (512,512)
-        self.img_dest_dim = (128, 128)
-        self.img_pos = (WIDTH / 2, 2*HEIGHT / 3.)
-        self.img_rot = 0
+        
+        self.vel = Vector()
+
+        self.width_height = Vector(self.frame_width, self.frame_height)        
+        self.pos = Vector(0,0)
+        
+        
+        self.radius = max(radius, 10)
         self.clockwise = None
         self.health = 100
         self.wrapped = False 
         self.distance = 0
         self.dead = False
         
+    def next_frame(self):
+        self.frame_index[0] = (self.frame_index[0] + 1) % self.COLUMNS
+        if self.frame_index[0] == 0:
+            self.frame_index[1] = (self.frame_index[1] + 1) % self.ROWS
 
-    def draw(self, canvas):
-        canvas.draw_image(self.img, self.img_centre, self.img_dims, self.pos.get_p(), (self.radius, self.radius), self.img_rot)
-        canvas.draw_text(('Distance:' + str(self.distance)), (20, 50), 50, 'Blue')
+    def last_frame(self):
+        self.frame_index[0] = (self.frame_index[0] - 1) % self.COLUMNS
+        if self.frame_index[0] == 0:
+            self.frame_index[1] = (self.frame_index[1] - 1) % self.ROWS
+
 
 
     def update(self):
-        self.rotate()
         self.pos.add(self.vel)
         self.vel.multiply(0.85)
         self.wrap()
+        if self.pos.x > WIDTH:
+            self.vel.multiply(-1.5)
+            
+        if self.pos.x < 0:
+            self.vel.multiply(-1.5)
+            
+        if self.pos.y > HEIGHT:
+            self.vel.multiply(-1.5)         
+            
+        if self.pos.y < 0:
+            self.vel.multiply(-1.5)
+            
         if self.ground_check() == False:
             self.vel.y += GRAVITY
         else:
@@ -52,16 +88,7 @@ class Wheel:
             self.distance += 1
           
            
-            
-
-    def rotate(self):
-        if self.clockwise is not None:
-            if self.clockwise == True:
-                self.img_rot -= STEP
-            else:
-                self.img_rot += STEP
-        else:
-            self.img_rot = 0
+           
 
 
     def wrap(self):
@@ -69,8 +96,6 @@ class Wheel:
         if position[0] + self.radius > WIDTH: #wrap r -> l
             self.pos = Vector(0 + self.radius, position[1])
             self.wrapped = True
-        elif position[0] + self.radius < 0:
-            self.vel.multiply(-5)
         else:
             self.wrapped = False
 
@@ -92,7 +117,7 @@ class Wheel:
         self.dead = False
         self.health = 100
         self.distance = 0
-        self.pos = Vector(WIDTH - 20, HEIGHT - 20)
+        self.pos = Vector(0,350)
             
         
     def bounce(self, normal):
@@ -175,58 +200,31 @@ class circle:
         perpendicular = self.pos.copy().subtract(ball.pos)
         return perpendicular.normalize()
     
+class clock:
+    def __init__(self, time):
+        self.time = time
+        
+    def tick(self):
+        self.time = self.time + 1
+            
+    def transition(self, frame_duration):
+        if self.time % frame_duration == 0:
+            return True
+        else:
+            return False
+        
 class Interaction:
 
-    def __init__(self, wheel, keyboard, menu):
+    def __init__(self, wheel, keyboard, menu, clock):
         self.wheel = wheel
+        self.clock = clock
         self.keyboard = keyboard
         self.menu = menu
         self.collision = False
         self.circles = []
         self.projectiles = []
-       
+   
 
-    def update(self):
-        if menu.active == False:
-            if self.keyboard.right:
-                self.wheel.vel.add(Vector(1, 0))
-                self.wheel.clockwise = True
-            elif self.keyboard.left:
-                self.wheel.vel.add(Vector(-1,0))
-                self.wheel.clockwise = False
-            elif self.keyboard.space:
-                if self.wheel.ground_check() == True:
-                    self.keyboard.space = False
-                    self.wheel.vel.add(Vector(0,-30))
-
-                self.wheel.clockwise = None
-                
-            else:
-                self.wheel.clockwise = None
-            for circle in self.circles:
-                  if circle.hit(wheel) == True:
-                        if not self.collision:
-                            wheel.take_damage()
-                            normal = circle.normal(wheel)
-                            self.wheel.bounce(normal) 
-                            self.collision = True
-                        else:
-                            self.collision = False
-            if wheel.wrapped == True:
-               self.circles = []
-               self.generate()
-               self.projectiles = []       
-            if wheel.dead == True:
-               menu.scores.append(wheel.distance)
-               self.circles = []
-               menu.set_active()    
-        elif menu.active == True:
-           if self.keyboard.p:
-               wheel.reset_wheel()
-               self.generate()
-               menu.set_inactive()
-           elif self.keyboard.q:
-               quit()
            
                       
     def generate(self):
@@ -249,29 +247,80 @@ class Interaction:
             i = circle(Vector(x,y),radius,7,"blue")
             self.projectiles.append(i)    
     
-        
-    
+    def draw(self, canvas):
+        centre = (self.wheel.frame_width * self.wheel.frame_index[0] + self.wheel.frame_centre_x,
+                self.wheel.frame_height * self.wheel.frame_index[1] + self.wheel.frame_centre_y)
+        if menu.active == False:
+            
+            if self.keyboard.right and self.clock.transition(time):
+                self.wheel.vel.add(Vector(1, 0))
+                self.wheel.next_frame()
+                self.wheel.update()
+                self.wheel.clockwise = True
+                
+            if self.keyboard.left and self.clock.transition(time):
+                self.wheel.vel.add(Vector(-1,0))
+                self.wheel.last_frame()
+                self.wheel.update()
+                self.wheel.clockwise = False
+                
+            if self.keyboard.space:
+                if self.wheel.ground_check() == True:
+                    self.keyboard.space = False
+                    self.wheel.vel.add(Vector(0,-50))
+                    self.wheel.next_frame()
+                    self.wheel.update()
+
+                self.wheel.clockwise = None
+                
+            for circle in self.circles:
+                if circle.hit(wheel) == True:
+                    if not self.collision:
+                        wheel.take_damage()
+                        normal = circle.normal(wheel)
+                        self.wheel.bounce(normal) 
+                        self.collision = True
+                    else:
+                         self.collision = False
+            if wheel.wrapped == True:
+                self.circles = []
+                self.generate()
+                self.projectiles = []    
+                
+            if wheel.dead == True:
+                menu.scores.append(wheel.distance)
+                self.circles = []
+                menu.set_active() 
+                
+            wheel.update()
+            dest_size = (100,100)
+            self.clock.tick()
+            canvas.draw_text(('Distance:' + str(self.wheel.distance)), (20, 50), 50, 'Blue')
+            canvas.draw_image(self.wheel.SHEET_URL, centre, self.wheel.width_height.get_p(), self.wheel.pos.get_p(), dest_size)
+            circles = self.circles
+            for circle in circles:
+                circle.draw(canvas)
+   
+        else:
+            if self.keyboard.p:
+                wheel.reset_wheel()
+                self.generate()
+                menu.set_inactive()
+            elif self.keyboard.q:
+                quit()
+            menu.draw(canvas)
          
 
 kbd = Keyboard()
-wheel = Wheel(Vector(WIDTH - 20, HEIGHT-20), 40)
+wheel = Wheel(SHEET_URL, SHEET_WIDTH, SHEET_HEIGHT, COLUMNS, ROWS)
 menu = Menu()
-inter = Interaction(wheel, kbd, menu)
+clock = clock(time)
+inter = Interaction(wheel, kbd, menu, clock)
 
-def draw(canvas):
-    if menu.active == False:
-        inter.update()
-        wheel.update()
-        wheel.draw(canvas)
-        circles = inter.circles
-        for circle in circles:
-            circle.draw(canvas)
-    else:
-        inter.update()
-        menu.draw(canvas)
+
     
 frame = simplegui.create_frame('Interactions', WIDTH, HEIGHT)
-frame.set_draw_handler(draw)
+frame.set_draw_handler(inter.draw)
 frame.set_canvas_background('#31CAF3')
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
